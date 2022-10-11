265d760f89ede917db09e48ac2341a88d1fca8ec
maribelniok <mariannabelniok@outlook.com>
2022-10-11 20:36:52 -0300

    ADD: text component

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,33 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Text, TextProps } from "./Text";
+
+export default {
+  title: "Components/Text",
+  component: Text,
+  args: {
+    children: "Texto",
+  },
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg'],
+      control: {
+        type: 'inline-radio',
+      },
+    }
+  },
+} as Meta<TextProps>;
+
+export const Default: StoryObj<TextProps> = {};
+export const CustomComponent: StoryObj<TextProps> = {
+  args: {
+    asChild: true,
+    children: <p>Testando</p>,
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+};
