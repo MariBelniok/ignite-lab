a54eca26ea01e2cc55aa965e0bdd8383dd7f4043
maribelniok <mariannabelniok@outlook.com>
2022-10-14 18:24:51 -0300

    add: initial form

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,22 @@
 import { clsx } from 'clsx';
-import { ReactNode } from 'react';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
 import { Slot } from '@radix-ui/react-slot';
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	size?: 'sm' | 'md' | 'lg',
 	children: ReactNode;
 	asChild?: boolean;
+  className?: string;
 }
 
-export function Button({ size = 'md', children, asChild }: ButtonProps) {
+export function Button({ size = 'md', children, asChild, className }: ButtonProps) {
 	const Comp = asChild ? Slot : 'button';
 
   return (
 		<Comp 
 			className={clsx(
-				'py-4 px-3 bg-cyan-500 rounded font-semibold text-black text-sm w-full hover:bg-cyan-300 transition-colors',
+				'py-3 px-4 bg-cyan-500 rounded font-semibold text-black text-sm w-full hover:bg-cyan-300 transition-colors',
+        className
 			)}
 		>
 			{ children }
