d6e83e19a7c6fccf7a80d45dc4b2062b8ee9e46b
maribelniok <mariannabelniok@outlook.com>
2022-10-14 19:17:34 -0300

    add: signin page

---
+++ src/pages/Signin.stories.tsx
@@ -0,0 +1,11 @@
+import { Signin } from "./Signin";
+
+export default {
+  title: "Pages/Signin",
+  component: Signin,
+  args: {
+    children: "Continuar",
+  },
+};
+
+export const Default = {};
